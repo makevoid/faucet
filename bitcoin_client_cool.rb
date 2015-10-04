42a9099ad49db5b1b1002f3eaf108732d82b2575
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2015-10-04 15:19:19 +0100

    status: hackathons are cool but ... :D

--- bitcoin_client_cool.rb
+++ bitcoin_client_cool.rb
@@ -14,9 +14,9 @@ puts "see #{url = 'https://en.bitcoin.it/wiki/Original_Bitcoin_client/API_calls_
 # BC.balance "label|address"
 
 
-# Generate new address 
+# Generate new address
 #
-# BC.generatenewaddress() # => "1..." (returns an address) 
+# BC.generatenewaddress() # => "1..." (returns an address)
 
 
 
@@ -33,20 +33,16 @@ puts "see #{url = 'https://en.bitcoin.it/wiki/Original_Bitcoin_client/API_calls_
 
 ### Send
 
-# BC.sendtoaddress "1MjTBm2TeHPAaXJnNGu6MyFqFnVm8g7Ys7", 0.001 # => "a2a2eb18cb051b5fe896a32b1cb20b179d981554b6bd7c5a956e56a0eecb04f1" 
+# BC.sendtoaddress "1MjTBm2TeHPAaXJnNGu6MyFqFnVm8g7Ys7", 0.001 # => "a2a2eb18cb051b5fe896a32b1cb20b179d981554b6bd7c5a956e56a0eecb04f1"
 #
 # # that is the transaction hash
 
 
 
-### secure the wallet with (optional but good, it's like putting a lock pin, except you should put a wrong password and adds a timed access/log-in in to your app, bitcoind in this case, to release the funds)
+### secure the wallet with (optional but good, it's like putting a lock pin, except that to make it clientside, you should put a wrong password and adds a timed access/log-in in to your app, bitcoind in this case, to release the funds)
 
 # walletlock
 
-# and 
+# and
 
 # walletpassphrase | walletpassphrasechange
-
-
-
-
