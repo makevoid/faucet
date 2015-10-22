ac474d3ccbf660dd4c9cc702838f452636150084
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2015-10-22 09:37:30 +0100

    >>

--- config/env.rb
+++ config/env.rb
@@ -11,15 +11,13 @@ RPC_HOST = 'localhost'
 RPC_HOST = '212.47.236.93' # bcc - scaleway
 
 
-unless APP_ENV == "development"
-  file = File.read File.expand_path( "~/.bitcoin/#{bitcoin_conf}" )
-  password = file.strip.match(/rpcpassword=(.+)/)[1]
+RPC_PASSWORD = unless APP_ENV == "development"
+  # file = File.read File.expand_path( "~/.bitcoin/bitcoin.conf" )
+  # password = file.strip.match(/rpcpassword=(.+)/)[1]
+  File.read( File.expand_path "~/.bitcoin/faucet.pass" ).strip
 else
-  password = File.read( File.expand_path "#{APP_PATH}/.bitcoin_rpc_password" ).strip
+  File.read( File.expand_path "#{APP_PATH}/.bitcoin_rpc_password" ).strip
 end
-
-RPC_PASSWORD = password
-
 
 DEBUG = false
 
